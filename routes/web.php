438cfdf654c9b57356be0ddc1b7a7b4ec75e724e
Hoang Nguyen <kza10303@gmail.com>
2024-01-30 21:28:24 +0700

    doing register form with MVC

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/register', [RegistrationController::class, 'display'])->name('authentication.register');
+Route::post('/handle-register', [RegistrationController::class, 'store'])->name('handle_register');
