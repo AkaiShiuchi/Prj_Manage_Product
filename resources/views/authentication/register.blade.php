2eb474fca193d1039e4f81b5e8bc51a9e6470b3a
Hoang <hoang@gmail.com>
2024-02-16 11:15:41 +0700

    tao giao dien register chua co send mail

--- resources/views/authentication/register.blade.php
+++ resources/views/authentication/register.blade.php
@@ -3,39 +3,69 @@
 @section('title', 'Login & Registration Form - W3hubs.com')
 
 @section('style')
-<link rel="stylesheet" href="{{asset('css/register.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/register.css') }}">
 @endsection
 
 @section('content')
-<div class="container">
-    <h1>Registration Form</h1>
-    <div class="row">
-        <div class="col-md-6">
-            <div id="sign">
-                <form action="/handle-register" method="POST">
-                    <h2>Registration</h2>
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Username" required>
+    <div class="container">
+        <div class="d-flex justify-content-center h-100">
+            <div class="card">
+                <div class="card-header">
+                    <h3>Sign Up</h3>
+                    <div class="d-flex justify-content-end social_icon">
+                        <span><i class="fab fa-facebook-square"></i></span>
+                        <span><i class="fab fa-google-plus-square"></i></span>
+                        <span><i class="fab fa-twitter-square"></i></span>
                     </div>
+                </div>
+                <div class="card-body">
+                    <form action="/handle-register" method="POST">
+                        @csrf
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </div>
+                        @endif
 
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password" required>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-user"></i></span>
+                            </div>
+                            <input type="text" class="form-control" placeholder="username" name="name" required>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-envelope"></i></span>
+                            </div>
+                            <input type="email" class="form-control" placeholder="email" name="email" required>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" class="form-control" placeholder="password" name="password" required>
+                        </div>
+                        <div class="input-group form-group">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text"><i class="fas fa-key"></i></span>
+                            </div>
+                            <input type="password" class="form-control" placeholder="confirm password"
+                                name="confirm_password" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" value="Sign Up" class="btn float-right login_btn">
+                        </div>
+                    </form>
+                </div>
+                <div class="card-footer">
+                    <div class="d-flex justify-content-center links">
+                        Already have an account? <a href="/">Sign In</a>
                     </div>
-
-                    <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Confirm Password" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="date" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <input type="email" class="form-control" placeholder="Email" required>
-                    </div>
-
-                    <button type="submit" class="btn btn-dark">Registration</button>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
 @endsection
